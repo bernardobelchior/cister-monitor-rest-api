98b5fcba2a2281caec4cfb46886cd6cba1afe92a
Bernardo Belchior <bernardo.belchior1@gmail.com>
2017-10-07 19:18:57 +0100

    First commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const mysql = require('mysql')
+const config = require('config.json')('config.json')
+
+const app = express()
+const connection = mysql.createConnection({
+    host: config.host,
+    user: config.user,
+    password: config.password,
+    database: config.database
+})
+
+connection.connect()
+
+app.get('/measurements/:numResults', function (req, res) {
+    var sql = 'SELECT * FROM Measurements ORDER BY timestamp DESC LIMIT ?'
+    var inserts = [req.params['numResults']]
+    sql = mysql.format(sql, inserts)
+
+    connection.query(sql, function (error, results, fields) {
+        res.send({
+            error,
+            results,
+            fields
+        })
+    })
+})
+
+app.listen(3000)
